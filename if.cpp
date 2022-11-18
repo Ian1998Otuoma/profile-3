17a751ab0dad7e7bff6d408b57a5cfad2f988a57
Ian1998Otuoma <115020729+Ian1998Otuoma@users.noreply.github.com>
2022-11-18 18:00:04 +0300

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Enter number the age: ";
+    cin >> age;
+
+    if(age>=4){
+        
+        cout << "admit to school";
+    } 
+    return 0;
+}
